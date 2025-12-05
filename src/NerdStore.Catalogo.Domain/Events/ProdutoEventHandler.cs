98622b757f3261c9df9005058ed59e7195e5db4c
LucasDev44 <lucas.dev44@gmail.com>
2025-12-05 19:01:06 -0300

    Adicionando Evendos de domínio

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,22 @@
+﻿
+
+using MediatR;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    // Classe de manipulação
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
+        }
+    }
+}
---
+++ src/NerdStore.Core/DomainObjects/DomainEvent.cs
@@ -0,0 +1,15 @@
+﻿
+
+using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public class DomainEvent : Event
+    {
+
+        public DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
